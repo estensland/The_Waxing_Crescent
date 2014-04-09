5b680e1587d429ee0e5b87f63570c8bb3a6f5a4e
Eric Stensland <estensland88@gmail.com>
2014-04-09 07:21:36 -0500

    adding travis ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,14 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'rspec/autorun'
+require "capybara/rails"
+require 'database_cleaner'
+
+require 'coveralls'
+require "factory_girl_rails"
+
+Coveralls.wear!('rails')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
